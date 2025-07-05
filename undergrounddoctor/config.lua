17dfab1ee0d491d3eb00ef45da941234f353f6b5
JoachimAB <joachim.andreassen08@gmail.com>
2025-07-05 19:19:32 +0200

    Update config.lua

--- undergrounddoctor/config.lua
+++ undergrounddoctor/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.NpcModel = 's_m_m_doctor_01'   -- Modelnavn på NPC 
-Config.NpcCoords = vector4(356.43, -593.36, 28.77, 67.12) -- Lokasjon på PED
+Config.NpcCoords = vector4(1397.6052, 3611.7029, 39.1397, 116.8931) -- Lokasjon på PED
 
 Config.RevivePrice = 15000         -- Kostnad for å bli revivet
 Config.ReviveTime = 60             -- Tid (sekunder) det tar å bli revivet
